e52c1a9fd3750a533a7d063ee49658d8fcd5ac32
Lene Julien <julien.lene@gmail.com>
2019-11-22 18:40:57 +0100

    crotte

--- app.py
+++ app.py
@@ -9,10 +9,10 @@ from portfolio_creation import get_asset
 # 12 sharp
 # 10 volatility
 if __name__ == '__main__':
-	tmp = "{'ratio'=[9, 12, 18],'asset'=[2201],'start_date'='2013-06-14','end_date'='2019-04-18','frequency':null}"
-	r = post_data('ratio/invoke', tmp)
-	#r = get_data("asset?columns=ASSET_DATABASE_ID&columns=TYPE&columns=LABEL")
-	print(r)
-	#r = tools.convert_to_EUR("USD", 2)
-	#print(r)
-	#get_asset()
+    tmp = "{'ratio'=[9, 12, 10, 13],'asset'=[2201],'start_date'='2013-06-14','end_date'='2019-04-18','frequency':null}"
+    r = post_data('ratio/invoke', tmp)
+    # r = get_data("asset?columns=ASSET_DATABASE_ID&columns=TYPE&columns=LABEL")
+    print(r)
+    # r = tools.convert_to_EUR("USD", 2)
+    # print(r)
+    get_asset()
--- portfolio_creation.py
+++ portfolio_creation.py
@@ -1,9 +1,11 @@
 import requests
 from tools import get_data
+from tools import post_data
 import json
 
 def request_asset_list():
-    r = get_data("asset?columns=ASSET_DATABASE_ID&columns=TYPE&columns=LABEL&coulmns=")
+    r = get_data("asset?columns=ASSET_DATABASE_ID&columns=TYPE&columns=LABEL&columns=LAST_CLOSE_VALUE_IN_CURR",
+                 start_date='2013-06-14', end_date='2019-04-18')
     dic_asset = json.loads(r)
     return dic_asset
 
@@ -12,6 +14,7 @@ def asset_sort(dic_asset):
     funds = []
     indexs = []
     for data in dic_asset:
+        print(data)
         if data['TYPE']['value'] == 'STOCK':
             stocks.append(data)
         elif data['TYPE']['value'] == 'FUND':
@@ -25,10 +28,21 @@ def asset_sort(dic_asset):
 def asset_id_sort(assets):
     for data in assets:
         print(data['ASSET_DATABASE_ID']['value'])
-        r = get_data("asset/" + data['ASSET_DATABASE_ID']['value'] + "?columns=CURRENCY", start_date='2013-06-14', end_date='2019-04-18')
+        r = get_data("asset/" + data['ASSET_DATABASE_ID']['value'] + "?columns=CURRENCY")
 
+def get_value_asset(assets):
+    res = []
+    for data in assets:
+        tmp = "{'ratio'=[9, 12, 10, 13],'asset'=[" + data['ASSET_DATABASE_ID']['value'] + "],'start_date'='2013-06-14','end_date'='2019-04-18','frequency':null}"
+        r = post_data('ratio/invoke', tmp)
+        value_dic = json.loads(r)
+        res.append(value_dic)
+    print(res)
+    return res
 
 def get_asset():
     asset_dict = request_asset_list()
     stocks, funds, indexs = asset_sort(asset_dict)
-    asset_id_sort(stocks)
+    get_value_asset(stocks)
+    get_value_asset(funds)
+    get_value_asset(indexs)
