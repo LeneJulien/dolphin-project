8fe88bc679b89e3bf999f8927a948cb544bc0b1f
Alexandre Buhl <alexandre.buhl@epita.fr>
2019-11-22 17:38:31 +0100

    push post function

--- app.py
+++ app.py
@@ -1,13 +1,15 @@
 import requests
 from config import URL, AUTH
+from tools import post_data
 import tools
 from portfolio_creation import get_asset
 
 
 if __name__ == '__main__':
-    #r = get_data("asset", "2013-04-26", "2014-04-26")
-    #r = get_data("asset?columns=ASSET_DATABASE_ID&columns=TYPE&columns=LABEL")
-    #print(r)
-    #r = tools.convert_to_EUR("USD", 2)
-    #print(r)
-    get_asset()
+	tmp = "{'ratio'=[9, 12, 18],'asset'=[2201],'bench'=null,'startDate'='2013-06-14','endDate'='2019-05-31','frequency':null}"
+	r = post_data('ratio/invoke', tmp)
+	#r = get_data("asset?columns=ASSET_DATABASE_ID&columns=TYPE&columns=LABEL")
+	print(r)
+	#r = tools.convert_to_EUR("USD", 2)
+	#print(r)
+	#get_asset()
--- tools.py
+++ tools.py
@@ -8,6 +8,11 @@ def get_data(endpointApi, start_date=None, end_date=None, full_response=False, c
     return res.content.decode('utf-8')
 
 
+def post_data(endpointApi, data=None, start_date=None, end_date=None, full_response=False, columns=list()):
+    payload = {'start_date': start_date, 'end_date': end_date, 'fullResponse': full_response}
+    res = requests.post(URL + endpointApi, data, params=payload, auth=AUTH, verify=False)
+    return res.content.decode('utf-8')
+
 def convert_to_EUR(identifier, value, date=None, full_response=False):
     payload = {'date': date, 'fullResponse': full_response}
     res = requests.get(URL + 'currency/rate/' + identifier + '/to/EUR', params=payload, auth=AUTH, verify=False)
