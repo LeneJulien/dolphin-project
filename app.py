63b2615b71fea6902adfec98e16b5e71767b4835
Alexandre Buhl <alexandre.buhl@epita.fr>
2019-11-23 20:11:12 +0100

    optimization algorithm

--- app.py
+++ app.py
@@ -1,18 +1,22 @@
 import requests
 from config import URL, AUTH
-from tools import post_data, get_data
+from tools import post_data, get_data, put_data
 import tools
 from portfolio_creation import get_asset
 
-# 9 rendement annualisé
+# 9 rendement annualise
 # 13 rendement
 # 12 sharp
 # 10 volatility
 if __name__ == '__main__':
-    tmp = "{'ratio'=[9, 12, 10, 13],'asset'=[2201],'start_date'='2013-06-14','end_date'='2019-04-18','frequency':null}"
-    r = post_data('ratio/invoke', tmp)
-    # r = get_data("asset?columns=ASSET_DATABASE_ID&columns=TYPE&columns=LABEL")
-    print(r)
+    tmp = "{'ratio'=[9, 12, 10, 13],'asset'=[1829],'start_date'='2013-06-14','end_date'='2019-04-18','frequency':null}"
+    put = "{'label':'EPITA_PTF_10','currency':{'code':'EUR'},'type':'front','values':{'2013-06-14':[{'asset':{'asset':2142,'quantity':1000.0}},{'asset':{'asset':2143,'quantity':2000.0}},{'asset':{'asset':2132,'quantity':5000.0}},{'asset':{'asset':2188,'quantity':90000.0}},{'asset':{'asset':2112,'quantity':7000.0}},{'asset':{'asset':1980,'quantity':7000.0}},{'asset':{'asset':2064,'quantity':7000.0}},{'asset':{'asset':2144,'quantity':7000.0}},{'asset':{'asset':2187,'quantity':7000.0}},{'asset':{'asset':1968,'quantity':7000.0}},{'asset':{'asset':2191,'quantity':7000.0}},{'asset':{'asset':2147,'quantity':7000.0}},{'asset':{'asset':1897,'quantity':7000.0}},{'asset':{'asset':1877,'quantity':7000.0}},{'asset':{'asset':2189,'quantity':7000.0}}]}}"
+    #r = post_data('ratio/invoke', tmp)
+    #r = put_data('portfolio/1829/dyn_amount_compo', put)
+    #r = get_data("portfolio/1829/dyn_amount_compo")
+    #print(r)
     # r = tools.convert_to_EUR("USD", 2)
     # print(r)
-    get_asset()
+    #get_asset()
+    tools.optimize()
+    
--- tools.py
+++ tools.py
@@ -1,6 +1,7 @@
 from config import URL, AUTH
 import requests
 import json
+from scipy.optimize import minimize
 
 def get_data(endpointApi, start_date=None, end_date=None, full_response=False, columns=list()):
     payload = {'start_date': start_date, 'end_date': end_date, 'fullResponse': full_response}
@@ -13,8 +14,49 @@ def post_data(endpointApi, data=None, start_date=None, end_date=None, full_respo
     res = requests.post(URL + endpointApi, data, params=payload, auth=AUTH, verify=False)
     return res.content.decode('utf-8')
 
+def put_data(endpointApi, data=None, start_date=None, end_date=None, full_response=False, columns=list()):
+    payload = {'start_date': start_date, 'end_date': end_date, 'fullResponse': full_response}
+    res = requests.put(URL + endpointApi, data, params=payload, auth=AUTH, verify=False)
+    return res.content.decode('utf-8')
+
 def convert_to_EUR(identifier, value, date=None, full_response=False):
     payload = {'date': date, 'fullResponse': full_response}
     res = requests.get(URL + 'currency/rate/' + identifier + '/to/EUR', params=payload, auth=AUTH, verify=False)
     r = json.loads(res.content.decode())
     return float(r['rate']['value'].replace(',', '.', 1)) * value
+
+
+def objectives(x):
+	x1 = x[0] * 2.0
+	x2 = x[1] * 1.4
+	x3 = x[2] * 1.9
+	x4 = x[3] * 1.8
+	x5 = x[4] * 1.4
+	x6 = x[5] * 1.4
+	x7 = x[6] * 1.0
+	x8 = x[7] * 1.47
+	x9 = x[8] * 1.45
+	x10 = x[9] * 1.9
+	x11 = x[10]
+	x12 = x[11]
+	x13 = x[12]
+	x14 = x[13]
+	x15 = x[14]
+	x16 = x[15]
+	x17 = x[16]
+	x18 = x[17]
+	x19 = x[18]
+	x20 = x[19]
+	return (x1 + x2 + x3 + x4 + x5 + x6 + x7 + x8 + x9 + x10 + x11 + x12 + x13 + x14 + x15 + x16 + x17 + x18 + x19 + x20)* 1
+
+def constraint(x):
+	return x[0] + x[1] + x[2] + x[3] + x[4] + x[5] + x[6] + x[7] + x[8] + x[9] + x[10] + x[11] + x[12] + x[13] + x[14] + x[15] + x[16] + x[17] + x[18] + x[19] - 100
+
+def optimize():
+	x0 = [1, 1, 1, 1, 1, 1, 2, 2, 2, 3, 3, 3, 4, 4, 4, 5, 5, 5, 6, 6]
+	print(len(x0))
+	b = (1.0, 10.0)
+	bnds = (b, b, b, b, b, b, b, b, b, b, b, b, b, b, b, b, b, b, b, b)
+	cons = [{'type': 'ineq', 'fun': constraint}]
+	solution = minimize(objectives, x0, method='SLSQP', bounds=bnds, constraints=cons)
+	print(solution)
